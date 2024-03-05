60ad23dbce89cc2c0d7b756b5ed8ac48a1da1223
sekizlipenguen <muratdemir3475@gmail.com>
2024-03-05 12:23:54 +0300

    connectType = fetch or null fetch progress(function does not work: progress)

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,16 @@
 declare module "@sekizlipenguen/connection" {
+    export interface Config {
+        connectType?: string
+    }
 
+    export interface connection {
+        get: (url: string, config: Config) => void,
+        post: (url: string, data?: object, config?: Config) => void,
+        put: (url: string, data?: object, config?: Config) => void,
+        patch: (url: string, data?: object, config?: Config) => void,
+        delete: (url: string, config: Config) => void,
+        setConfig: (config: Config) => void
+    }
+
+    export const connection: connection;
 }
