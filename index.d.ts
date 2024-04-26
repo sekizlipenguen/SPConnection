5663c7b7540ff64fac24e306387e2eb3a599bfbb
sekizlipenguen <muratdemir3475@gmail.com>
2024-04-26 11:58:57 +0300

    types v6

--- index.d.ts
+++ index.d.ts
@@ -4,15 +4,22 @@ declare module "@sekizlipenguen/connection" {
 
     export interface Config {
         connectType?: string,
-        headers?: object
+        headers?: object,
+        timeout?: number
+    }
+
+    export interface ReturnTypeConfig {
+        data?: any,
+        request?: any,
+        statusCode?: number
     }
 
     export interface connection {
-        get: (url: string, config: Config) => Promise<any>,
-        post: (url: string, data?: object, config?: Config) => Promise<any>,
-        put: (url: string, data?: object, config?: Config) => Promise<any>,
-        patch: (url: string, data?: object, config?: Config) => Promise<any>,
-        delete: (url: string, config: Config) => Promise<any>,
+        get: (url: string, config?: Config) => Promise<ReturnTypeConfig>,
+        post: (url: string, data?: object, config?: Config) => Promise<ReturnTypeConfig>,
+        put: (url: string, data?: object, config?: Config) => Promise<ReturnTypeConfig>,
+        patch: (url: string, data?: object, config?: Config) => Promise<ReturnTypeConfig>,
+        delete: (url: string, config?: Config) => Promise<ReturnTypeConfig>,
         setConfig: (config: Config) => void
     }
 
