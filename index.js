591e9de76df7743830fcd70da9a7214aa30e276f
sekizlipenguen <muratdemir3475@gmail.com>
2025-10-24 11:29:58 +0300

    feat: Improve error handling and enhance fetch response normalization
    - Introduced `normalizeError` for unified error transformation, including status mapping.
    - Enhanced fetch and XMLHttpRequest response handling to normalize error and response objects.
    - Adjusted default configurations and logging for improved debugging.
    - Incremented package version to 0.2.4.

--- index.js
+++ index.js
@@ -8,162 +8,183 @@ const isObject = (v) => v && Object.prototype.toString.call(v) === '[object Obje
 const isArray = (v) => Array.isArray(v);
 
 const log = (...args) => {
-    if (logEnabled) {
-        console.info('[Connection Log]:', ...args);
-    }
+  if (logEnabled) {
+    console.info('[Connection Log]:', ...args);
+  }
+};
+
+const normalizeError = (error) => {
+  if (typeof error === 'object' && error.status) {
+    return {...error, statusCode: error.status};
+  }
+  return error;
 };
 
 const handleError = (error, reject) => {
-    if (error.statusCode === 408) {
-        reject({statusCode: 408, message: 'Timeout occurred'});
-    } else if (error.statusCode === 0) {
-        reject({statusCode: 0, message: 'Network disconnected'});
-    } else {
-        reject(error);
-    }
+  if (error.statusCode === 408) {
+    reject({statusCode: 408, message: 'Timeout occurred'});
+  } else if (error.statusCode === 0) {
+    reject({statusCode: 0, message: 'Network disconnected'});
+  } else {
+    reject(normalizeError(error));
+  }
 };
 
 const connect = (url, data = {}, config = {}) => {
-    log('XHR Connect Start:', {url, data, config});
+  log('XHR Connect Start:', {url, data, config});
 
-    let body = (isObject(data) || isArray(data)) && config.files !== true ? JSON.stringify(data) : data;
-    const defaultInit = {method: 'GET', timeout, progress: null, headers: {}, async: true};
-    const init = initProps(defaultInit, config);
+  let body = (isObject(data) || isArray(data)) && config.files !== true ? JSON.stringify(data) : data;
+  const defaultInit = {method: 'GET', timeout, progress: null, headers: {}, async: true};
+  const init = initProps(defaultInit, config);
 
-    return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest();
-        request.open(init.method, url, init.async);
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.open(init.method, url, init.async);
 
-        if (init.timeout) {
-            request.timeout = init.timeout;
-        }
+    if (init.timeout) {
+      request.timeout = init.timeout;
+    }
 
-        request.upload.addEventListener('progress', (e) => init.progress && init.progress(e));
+    request.upload.addEventListener('progress', (e) => init.progress && init.progress(e));
 
-        request.ontimeout = () => (request.isTimeout = true);
+    request.ontimeout = () => (request.isTimeout = true);
 
-        request.onreadystatechange = () => {
-            if (request.readyState !== 4) {
-                return;
-            }
+    request.onreadystatechange = () => {
+      if (request.readyState !== 4) {
+        return;
+      }
 
-            let result = request.response;
-            const contentType = request.getResponseHeader('Content-Type');
-            if (contentType && contentType.includes('application/json')) {
-                result = JSON.parse(request.response);
-            }
+      let result = request.response;
+      const contentType = request.getResponseHeader('Content-Type');
+      if (contentType && contentType.includes('application/json')) {
+        result = JSON.parse(request.response);
+      }
 
-            if (request.status === 0) {
-                setTimeout(() => {
-                    if (request.isTimeout) {
-                        request.isTimeout = false;
-                        handleError({statusCode: 408}, reject);
-                    } else {
-                        handleError({statusCode: 0}, reject);
-                    }
-                }, 1);
-            } else if (request.status >= 200 && request.status < 300) {
-                log('XHR Connect Success:', {result});
-                resolve({data: result, request, statusCode: request.status});
-            } else {
-                log('XHR Connect Error:', {status: request.status, response: result});
-                reject({data: result, request, statusCode: request.status});
-            }
-        };
+      if (request.status === 0) {
+        setTimeout(() => {
+          if (request.isTimeout) {
+            request.isTimeout = false;
+            handleError({statusCode: 408}, reject);
+          } else {
+            handleError({statusCode: 0}, reject);
+          }
+        }, 1);
+      } else if (request.status >= 200 && request.status < 300) {
+        log('XHR Connect Success:', {result});
+        resolve({data: result, request, statusCode: request.status});
+      } else {
+        log('XHR Connect Error:', {status: request.status, response: result});
+        reject({data: result, request, statusCode: request.status});
+      }
+    };
 
-        Object.keys(init.headers).forEach((key) => request.setRequestHeader(key, init.headers[key]));
-        request.send(body);
-    });
+    Object.keys(init.headers).forEach((key) => request.setRequestHeader(key, init.headers[key]));
+    request.send(body);
+  });
 };
 
 const connectFetch = (url, data = {}, config = {}) => {
-    log('Fetch Connect Start:', {url, data, config});
+  log('Fetch Connect Start:', {url, data, config});
 
-    let then1 = {};
-    let body = (isObject(data) || isArray(data)) && config.files !== true ? JSON.stringify(data) : data;
+  let then1 = {};
+  let body = (isObject(data) || isArray(data)) && config.files !== true ? JSON.stringify(data) : data;
 
-    const defaultInit = {method: 'GET', timeout, progress: null, headers: {}, async: true};
-    const init = initProps(defaultInit, config);
-    return new Promise((resolve, reject) => {
-        const controller = new AbortController();
-        const timeoutId = setTimeout(() => controller.abort(), init.timeout);
+  const defaultInit = {method: 'GET', timeout, progress: null, headers: {}, async: true};
+  const init = initProps(defaultInit, config);
+  return new Promise((resolve, reject) => {
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), init.timeout);
 
-        fetch(url, {
-            ...init,
-            ...(init.method !== 'GET' ? {body} : {}), // Sadece GET dışında body eklenir
-            signal: controller.signal,
-        }).then((response) => {
-            then1 = response;
-            const contentType = response.headers.get('content-type') || '';
-            if (contentType.includes('application/json')) {
-                return response.json();
-            } else if (contentType.includes('text/')) {
-                return response.text();
-            } else if (contentType.includes('application/octet-stream')) {
-                return response.blob();
-            }
-            return response;
-        }).then((res2) => {
-            const then2 = {data: res2};
-            const result = Object.assign({}, then2, then1, {config: init});
-            if (then1.ok) {
-                log('Fetch Connect Success:', {result});
-                resolve(result);
-            } else {
-                log('Fetch Connect Error:', {status: then1.status, response: res2});
-                reject(result);
-            }
-        }).catch((err) => {
-            log('Fetch Connect Catch Error:', err);
-            reject(err);
-        }).finally(() => clearTimeout(timeoutId));
-    });
+    fetch(url, {
+      ...init,
+      ...(init.method !== 'GET' ? {body} : {}), // Sadece GET dışında body eklenir
+      signal: controller.signal,
+    }).then((response) => {
+      then1 = response;
+      const contentType = response.headers.get('content-type') || '';
+      if (contentType.includes('application/json')) {
+        return response.json();
+      } else if (contentType.includes('text/')) {
+        return response.text();
+      } else if (contentType.includes('application/octet-stream')) {
+        return response.blob();
+      }
+      return response;
+    }).then((res2) => {
+      const then2 = {data: res2};
+      const result = Object.assign({}, then2, then1, {config: init});
+
+      if (then1.ok) {
+        log('Fetch Connect Success:', {result});
+        if (typeof res2 === 'object' && res2 !== null) {
+          resolve({...result, statusCode: then1.status});
+        } else {
+          resolve(result);
+        }
+      } else {
+        log('Fetch Connect Error:', {status: then1.status, response: res2});
+        if (typeof result.data === 'object') {
+          reject({...result, statusCode: result.status});
+        } else {
+          reject(result);
+        }
+      }
+    }).catch((err) => {
+      log('Fetch Connect Catch Error:', err);
+      if (typeof err === 'object' && err !== null) {
+        reject({...err, statusCode: err.status});
+      } else {
+        reject(err);
+      }
+    }).finally(() => clearTimeout(timeoutId));
+  });
 };
 
 class connection {
-    static request(method, url, data = {}, config = {}) {
-        const defaultInit = {method, connectType};
-        const init = initProps(defaultInit, config);
-        return init.connectType === 'fetch'
-            ? connectFetch(url, data, init)
-            : connect(url, data, init);
-    }
+  static request(method, url, data = {}, config = {}) {
+    const defaultInit = {method, connectType};
+    const init = initProps(defaultInit, config);
+    return init.connectType === 'fetch'
+        ? connectFetch(url, data, init)
+        : connect(url, data, init);
+  }
 
-    static get(url, config) {
-        return this.request('GET', url, undefined, config);
-    }
+  static get(url, config) {
+    return this.request('GET', url, undefined, config);
+  }
 
-    static post(url, data, config) {
-        return this.request('POST', url, data, config);
-    }
+  static post(url, data, config) {
+    return this.request('POST', url, data, config);
+  }
 
-    static put(url, data, config) {
-        return this.request('PUT', url, data, config);
-    }
+  static put(url, data, config) {
+    return this.request('PUT', url, data, config);
+  }
 
-    static patch(url, data, config) {
-        return this.request('PATCH', url, data, config);
-    }
+  static patch(url, data, config) {
+    return this.request('PATCH', url, data, config);
+  }
 
-    static delete(url, data, config) {
-        return this.request('DELETE', url, data, config);
-    }
+  static delete(url, data, config) {
+    return this.request('DELETE', url, data, config);
+  }
 
-    static setConfig(config) {
-        const defaultInit = {timeout, connectType};
-        const init = initProps(defaultInit, config);
-        if (init.timeout) {
-            timeout = init.timeout;
-        }
-        if (init.connectType) {
-            connectType = init.connectType;
-        }
+  static setConfig(config) {
+    const defaultInit = {timeout, connectType};
+    const init = initProps(defaultInit, config);
+    if (init.timeout) {
+      timeout = init.timeout;
     }
+    if (init.connectType) {
+      connectType = init.connectType;
+    }
+  }
 
-    static enableLogs(enable) {
-        logEnabled = enable;
-        log('Logging enabled:', enable);
-    }
+  static enableLogs(enable) {
+    logEnabled = enable;
+    log('Logging enabled:', enable);
+  }
 }
 
 export default connection;
+
